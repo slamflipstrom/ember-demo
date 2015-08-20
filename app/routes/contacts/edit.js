6769abd279cd823370dd3ab8477913832ced00f3
Sam Lindstrom <lindstrom.s@gmail.com>
2015-08-20 14:39:45 -0600

    Show/Edit action working. Redirect WIP
    Update button is not redirecting to Contacts index as desired.

--- app/routes/contacts/edit.js
+++ app/routes/contacts/edit.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  actions: {
-    updateContact: function(){
-      var contact = this.get('currentModel');
-
-      this.transitionTo('contacts', contact.save());
-    }
-  }
 });
