b25d0dc578fb3fb72d4f81e143cc11b95fe52994
Sam Lindstrom <lindstrom.s@gmail.com>
2017-07-16 15:58:09 -0600

    Update node for heroku

--- app/models/contact.js
+++ app/models/contact.js
@@ -2,15 +2,15 @@ import DS from "ember-data";
 
 //define the Contact model
 var Contact = DS.Model.extend({
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
-  address: DS.attr('string'),
-  city: DS.attr('string'),
-  state: DS.attr('string'),
-  country: DS.attr('string'),
-  zip: DS.attr('string'),
-  phone: DS.attr('string'),
-  email: DS.attr('string')
+  firstName: DS.attr("string"),
+  lastName: DS.attr("string"),
+  address: DS.attr("string"),
+  city: DS.attr("string"),
+  state: DS.attr("string"),
+  country: DS.attr("string"),
+  zip: DS.attr("string"),
+  phone: DS.attr("string"),
+  email: DS.attr("string")
 });
 
 //Create Contact fixtures
@@ -18,75 +18,75 @@ Contact.reopenClass({
   FIXTURES: [
     {
       id: 1,
-      firstName: 'John',
-      lastName: 'Elway',
-      address: '4529 Bronco Blvd',
-      city: 'Denver',
-      state: 'Colorado',
-      country: 'United States',
-      zip: '80205',
-      phone: '303-555-5555',
-      email: 'jelway@broncos.com'
+      firstName: "John",
+      lastName: "Elway",
+      address: "4529 Bronco Blvd",
+      city: "Denver",
+      state: "Colorado",
+      country: "United States",
+      zip: "80205",
+      phone: "303-555-5555",
+      email: "jelway@broncos.com"
     },
     {
       id: 2,
-      firstName: 'Leslie',
-      lastName: 'Knope',
-      address: '7233 Franklin St.',
-      city: 'Pawnee',
-      state: 'Indiana',
-      country: 'United States',
-      zip: '78136',
-      phone: '888-234-5555',
-      email: 'lknope@pawnee.net'
+      firstName: "Leslie",
+      lastName: "Knope",
+      address: "7233 Franklin St.",
+      city: "Pawnee",
+      state: "Indiana",
+      country: "United States",
+      zip: "78136",
+      phone: "888-234-5533",
+      email: "lknope@pawnee.net"
     },
     {
       id: 3,
-      firstName: 'Donald',
-      lastName: 'Trump',
-      address: '4529 Valley Ln',
-      city: 'Las Vegas',
-      state: 'Nevada',
-      country: 'United States',
-      zip: '48943',
-      phone: '555-555-5555',
-      email: 'trumpbamf@trump.com'
+      firstName: "Elon",
+      lastName: "Musk",
+      address: "4529 Tesla Blvd",
+      city: "Los Angeles",
+      state: "CA",
+      country: "United States",
+      zip: "48943",
+      phone: "901-837-5585",
+      email: "elon@spacex.com"
     },
     {
       id: 4,
-      firstName: 'Alex',
-      lastName: 'Morgan',
-      address: '7233 Franklin St.',
-      city: 'Los Angeles',
-      state: 'California',
-      country: 'United States',
-      zip: '90285',
-      phone: '651-777-5555',
-      email: 'amorgan@ussoccer.com'
+      firstName: "Alex",
+      lastName: "Morgan",
+      address: "7233 Franklin St.",
+      city: "Los Angeles",
+      state: "California",
+      country: "United States",
+      zip: "90285",
+      phone: "651-777-5555",
+      email: "amorgan@ussoccer.com"
     },
     {
       id: 5,
-      firstName: 'Laura',
-      lastName: 'Palmer',
-      address: '708 33rd St.',
-      city: 'Twin Peaks',
-      state: 'Washington',
-      country: 'United States',
-      zip: '34822',
-      phone: '206-823-4444',
-      email: 'bobbysgurl@loglady.com'
+      firstName: "Laura",
+      lastName: "Palmer",
+      address: "708 33rd St.",
+      city: "Twin Peaks",
+      state: "Washington",
+      country: "United States",
+      zip: "34822",
+      phone: "206-823-4444",
+      email: "bobbysgurl@loglady.com"
     },
     {
       id: 6,
-      firstName: 'Rick',
-      lastName: 'Grimes',
-      address: '9765 2nd Ave',
-      city: 'King County',
-      state: 'Georgia',
-      country: 'United States',
-      zip: '74211',
-      phone: '745-652-4412',
-      email: 'cuddlykitten@hotmail.com'
+      firstName: "Rick",
+      lastName: "Grimes",
+      address: "9765 2nd Ave",
+      city: "King County",
+      state: "Georgia",
+      country: "United States",
+      zip: "74211",
+      phone: "745-652-4412",
+      email: "cuddlykitten@hotmail.com"
     }
   ]
 });
--- config/environment.js
+++ config/environment.js
@@ -2,10 +2,10 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'effective-ui',
+    modulePrefix: "effective-ui",
     environment: environment,
-    baseURL: '/',
-    locationType: 'auto',
+    baseURL: "/",
+    locationType: "auto",
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
@@ -16,13 +16,10 @@ module.exports = function(environment) {
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-    sassOptions: {
-      includePaths: ['bower_components/materialize/sass']
     }
   };
 
-  if (environment === 'development') {
+  if (environment === "development") {
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
@@ -30,20 +27,19 @@ module.exports = function(environment) {
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
-  if (environment === 'test') {
+  if (environment === "test") {
     // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
+    ENV.baseURL = "/";
+    ENV.locationType = "none";
 
     // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-    ENV.APP.rootElement = '#ember-testing';
+    ENV.APP.rootElement = "#ember-testing";
   }
 
-  if (environment === 'production') {
-
+  if (environment === "production") {
   }
 
   return ENV;
